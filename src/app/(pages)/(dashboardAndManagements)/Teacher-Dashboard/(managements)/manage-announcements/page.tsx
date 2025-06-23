ac93bb7ba79e4392ee7a3e5ceed86e048c2ce8ed
Tamasru Pain <tamasrupain02@gmail.com>
2025-06-23 12:36:25 +0530

    tab addition

--- src/app/(pages)/(dashboardAndManagements)/Teacher-Dashboard/(managements)/manage-announcements/page.tsx
+++ src/app/(pages)/(dashboardAndManagements)/Teacher-Dashboard/(managements)/manage-announcements/page.tsx
@@ -33,13 +33,13 @@ export default function ManageAnnoucements() {
 
     return (
         <div className="">
-            <div className="m-4">
+            <div className="p-2 m-2">
                 <h1 className="text-2xl p-2">
                     <FontAwesomeIcon icon={faBullhorn} /> Management Announcments
                 </h1>
-                <div className="p-2">
+                <div className="">
                     {/* search bar */}
-                    <div className="flex items-center justify-center gap-3">
+                    <div className="flex items-center justify-center mt-2 gap-3">
                         <input
                             type="search"
                             className="input placeholder:text-gray-400 hover:shadow-md shadow-sky-400 w-64 p-2 rounded"
@@ -54,64 +54,96 @@ export default function ManageAnnoucements() {
                             Clear
                         </button>
                     </div>
-                    {/* add button */}
-                    <div className="flex items-center justify-between p-2 mt-2">
-                        <h1 className="text-lg hidden md:block">Announcements ( {annoucements.length} )</h1>
-                        <a href="#my_modal_8" className="btn btn-soft ml-4">
-                            <FontAwesomeIcon icon={faSquarePlus} /> Add Announcment
-                        </a>
-                    </div>
-                </div>
+                    {/* name of each tab group should be unique */}
+                    <div className="tabs tabs-lift mt-4">
+                        {/* tab 1 */}
+                        <input type="radio" name="my_tabs_3" className="tab" aria-label="Your announcements" defaultChecked />
+                        <div className="tab-content backdrop-blur-sm bg-black/10 border-base-300 p-2">
 
-                {/* list of announcement */}
-                <div className="mt-2">
-                    <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-4 p-4
+                            {/* add button */}
+                            <div className="flex items-center justify-between p-2 mt-2">
+                                <h1 className="text-lg hidden md:block">Announcements ( {annoucements.length} )</h1>
+                                <a href="#my_modal_8" className="btn btn-soft ml-4">
+                                    <FontAwesomeIcon icon={faSquarePlus} /> Add Announcment
+                                </a>
+                            </div>
+                            {/* list of announcement */}
+                            <div className="mt-2">
+                                <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-4 p-4
                                     backdrop-blur-xl
                                     h-screen overflow-scroll rounded-xl">
-                        {filteredAnnoucement.map(e => (
-                            <div className="bg-base-300 p-3 card hover:shadow shadow-blue-500" key={e.id}>
-                                <div className="flex items-center justify-between my-1">
-                                    <h1>{e.title}</h1>
-                                    <span className="text-gray-500">{e.time}</span>
-                                </div>
-                                <p className="text-gray-400">{e.description}</p>
-                                <div className="flex items-end justify-end">
-                                    <button className="btn btn-soft btn-primary mx-2 my-1">
-                                        <FontAwesomeIcon icon={faEye} />
-                                    </button>
-                                    <button className="btn btn-soft btn-success mx-2 my-1">
-                                        <FontAwesomeIcon icon={faPenToSquare} />
-                                    </button>
-                                    <button className="btn btn-soft btn-warning mx-2 my-1">
-                                        <FontAwesomeIcon icon={faTrashCan} />
-                                    </button>
+                                    {filteredAnnoucement.map(e => (
+                                        <div className="bg-base-300 p-3 card hover:shadow shadow-blue-500" key={e.id}>
+                                            <div className="flex items-center justify-between my-1">
+                                                <h1>{e.title}</h1>
+                                                <span className="text-gray-500">{e.time}</span>
+                                            </div>
+                                            <p className="text-gray-400">{e.description}</p>
+                                            <div className="flex items-end justify-end">
+                                                <button className="btn btn-soft btn-primary mx-2 my-1">
+                                                    <FontAwesomeIcon icon={faEye} />
+                                                </button>
+                                                <button className="btn btn-soft btn-success mx-2 my-1">
+                                                    <FontAwesomeIcon icon={faPenToSquare} />
+                                                </button>
+                                                <button className="btn btn-soft btn-warning mx-2 my-1">
+                                                    <FontAwesomeIcon icon={faTrashCan} />
+                                                </button>
+                                            </div>
+                                        </div>
+                                    ))}
                                 </div>
                             </div>
-                        ))}
+                        </div>
+                        {/* tab 2 */}
+                        <input type="radio" name="my_tabs_3" className="tab  [--tab-bg:]" aria-label="All annoucements" />
+                        <div className="tab-content backdrop-blur-sm bg-black/10 border-base-300 p-2">
+                            {/* list of announcement */}
+                            <div className="mt-2">
+                                <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-4 p-4
+                                    backdrop-blur-xl
+                                    h-screen overflow-scroll rounded-xl">
+                                    {filteredAnnoucement.map(e => (
+                                        <div className="bg-base-300 p-3 card hover:shadow shadow-blue-500" key={e.id}>
+                                            <div className="flex items-center justify-between my-1">
+                                                <h1>{e.title}</h1>
+                                                <span className="text-gray-500">{e.time}</span>
+                                            </div>
+                                            <p className="text-gray-400">{e.description}</p>
+                                            <div className="flex items-end justify-end">
+                                                <button className="btn btn-soft btn-primary mx-2 my-1">
+                                                    <FontAwesomeIcon icon={faEye} />
+                                                </button>
+                                            </div>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </div>
-            <div className="modal" role="dialog" id="my_modal_8">
-                <div className="modal-box">
-                    <div className="modal-action mt-1">
-                        <a href="#" className="btn">close</a>
-                    </div>
-                    <h3 className="text-lg font-bold">Hello!</h3>
-                    <div className="">
-                        <fieldset className="fieldset p-4">
+                    <div className="modal" role="dialog" id="my_modal_8">
+                        <div className="modal-box">
+                            <div className="modal-action mt-1">
+                                <a href="#" className="btn">close</a>
+                            </div>
+                            <h3 className="text-lg font-bold">Hello!</h3>
+                            <div className="">
+                                <fieldset className="fieldset p-4">
 
-                            <label className="label">Title</label>
-                            <input type="text" className="input w-full" placeholder="My awesome page" />
+                                    <label className="label">Title</label>
+                                    <input type="text" className="input w-full" placeholder="My awesome page" />
 
-                            <label className="label">Slug</label>
-                            <input type="text" className="input w-full" placeholder="my-awesome-page" />
+                                    <label className="label">Slug</label>
+                                    <input type="text" className="input w-full" placeholder="my-awesome-page" />
 
-                            <label className="label">Author</label>
-                            <input type="text" className="input w-full" placeholder="Name" />
+                                    <label className="label">Author</label>
+                                    <input type="text" className="input w-full" placeholder="Name" />
 
-                            <button className="btn btn-neutral mt-10">Submit</button>
+                                    <button className="btn btn-neutral mt-10">Submit</button>
 
-                        </fieldset>
+                                </fieldset>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
